5fa007a630a0a4fbea0e892ed15716f2f9938cbd
ddhan288 <ddhan288@gmail.com>
2024-03-23 14:40:37 +0530

    added all the resuseable component into the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 const App = () => {
   return (
     <>
@@ -42,23 +43,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings/>
-
-      {/* <!-- Hero --> */}
-
-      {/* <!-- Developers and Employers --> */}
-      
-
-      {/* <!-- Browse Jobs --> */}
-     
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs/>  
     </>
   );
 };
